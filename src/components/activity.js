fcebfa6ffe9dd3d4db99d294fabb046b62c727ad
Neha Bharati <nehabharati@Nehas-MacBook-Pro.local>
2020-07-25 16:07:44 +0530

    app done

--- src/components/activity.js
+++ src/components/activity.js
@@ -1,96 +1,99 @@
-const activity = 
-[
-    {
-		"id": "A1",
-		"real_name": "Egon Spengler",
-		"tz": "America/Los_Angeles",
-		"activity_periods": [{
-				"start_time": "Feb 1 2020  1:33PM",
-				"end_time": "Feb 1 2020 1:54PM"
-			},
-			{
-				"start_time": "Mar 1 2020  11:11AM",
-				"end_time": "Mar 1 2020 2:00PM"
-			},
-			{
-				"start_time": "Mar 16 2020  5:33PM",
-				"end_time": "Mar 16 2020 8:02PM"
-			}
-		]
-	},
-	{
-		"id": "A2",
-		"real_name": "Glinda Southgood",
-		"tz": "Asia/Kolkata",
-		"activity_periods": [{
-				"start_time": "Feb 1 2020  1:33PM",
-				"end_time": "Feb 1 2020 1:54PM"
-			},
-			{
-				"start_time": "Mar 1 2020  11:11AM",
-				"end_time": "Mar 1 2020 2:00PM"
-			},
-			{
-				"start_time": "Mar 16 2020  5:33PM",
-				"end_time": "Mar 16 2020 8:02PM"
-			}
-		]
-    },
-    {
-		"id": "A3",
-		"real_name": "Laurel Fischer",
-		"tz": "Asia/Kolkata",
-		"activity_periods": [{
-				"start_time": "Feb 1 2020  1:33PM",
-				"end_time": "Feb 1 2020 1:54PM"
-			},
-			{
-				"start_time": "Mar 1 2020  11:11AM",
-				"end_time": "Mar 1 2020 2:00PM"
-			},
-			{
-				"start_time": "Mar 16 2020  5:33PM",
-				"end_time": "Mar 16 2020 8:02PM"
-			}
-		]
-    },
-    {
-		"id": "A4",
-		"real_name": "Andrew Walsch",
-		"tz": "Asia/Kolkata",
-		"activity_periods": [{
-				"start_time": "Feb 1 2020  1:33PM",
-				"end_time": "Feb 1 2020 1:54PM"
-			},
-			{
-				"start_time": "Mar 1 2020  11:11AM",
-				"end_time": "Mar 1 2020 2:00PM"
-			},
-			{
-				"start_time": "Mar 16 2020  5:33PM",
-				"end_time": "Mar 16 2020 8:02PM"
-			}
-		]
-    },
-    {
-		"id": "A5",
-		"real_name": "Fred Flinstone",
-		"tz": "Asia/Kolkata",
-		"activity_periods": [{
-				"start_time": "Feb 1 2020  1:33PM",
-				"end_time": "Feb 1 2020 1:54PM"
-			},
-			{
-				"start_time": "Mar 1 2020  11:11AM",
-				"end_time": "Mar 1 2020 2:00PM"
-			},
-			{
-				"start_time": "Mar 16 2020  5:33PM",
-				"end_time": "Mar 16 2020 8:02PM"
-			}
-		]
-	}
-]
- 
+const activity = [
+  {
+    id: "A1",
+    real_name: "Egon Spengler",
+    tz: "America/Los_Angeles",
+    activity_periods: [
+      {
+        start_time: "May 1 2020  1:33PM",
+        end_time: "May 1 2020 1:54PM",
+      },
+      {
+        start_time: "Jul 1 2020  11:11AM",
+        end_time: "Jul 1 2020 2:00PM",
+      },
+      {
+        start_time: "Jun 16 2020  5:33PM",
+        end_time: "Jun 16 2020 8:02PM",
+      },
+    ],
+  },
+  {
+    id: "A2",
+    real_name: "Glinda Southgood",
+    tz: "Asia/Kolkata",
+    activity_periods: [
+      {
+        start_time: "May 13 2020  8:00AM",
+        end_time: "May 13 2020 10:00AM",
+      },
+      {
+        start_time: "Jul 21 2020  5:30AM",
+        end_time: "Jul 21 2020 7:00AM",
+      },
+      {
+        start_time: "Jun 6 2020  9:33PM",
+        end_time: "Jun 6 2020 9:53PM",
+      },
+    ],
+  },
+  {
+    id: "A3",
+    real_name: "Laurel Fischer",
+    tz: "Asia/Kolkata",
+    activity_periods: [
+      {
+        start_time: "May 20 2020  8:20PM",
+        end_time: "May 20 2020 9:54PM",
+      },
+      {
+        start_time: "Jul 16 2020  11:11AM",
+        end_time: "Jul 16 2020 12:00PM",
+      },
+      {
+        start_time: "Jun 14 2020  6:33PM",
+        end_time: "Jun 14 2020 8:02PM",
+      },
+    ],
+  },
+  {
+    id: "A4",
+    real_name: "Andrew Walsch",
+    tz: "Asia/Kolkata",
+    activity_periods: [
+      {
+        start_time: "May 12 2020  7:33AM",
+        end_time: "May 12 2020 10:44AM",
+      },
+      {
+        start_time: "Jul 4 2020  12:11PM",
+        end_time: "Jul 4 2020 2:00PM",
+      },
+      {
+        start_time: "Jun 5 2020  3:30PM",
+        end_time: "Jun 5 2020 6:30PM",
+      },
+    ],
+  },
+  {
+    id: "A5",
+    real_name: "Fred Flinstone",
+    tz: "Asia/Kolkata",
+    activity_periods: [
+      {
+        start_time: "May 30 2020  3:15AM",
+        end_time: "May 30 2020 5:30AM",
+      },
+      {
+        start_time: "Jul 10 2020  7:10AM",
+        end_time: "Jul 10 2020 8:00AM",
+      },
+      {
+        start_time: "Jun 22 2020  4:30PM",
+        end_time: "Jun 22 2020 5:00PM",
+      },
+    ],
+  },
+];
 
-export default activity
+export default activity;
--- src/components/resources.js
+++ src/components/resources.js
@@ -1,103 +1,102 @@
 const events = [
-    {
-        id: 0,
-        title: 'Egon Spengler',
-        start: new Date(2020, 1, 1, 13, 10, 0, 0),
-        end: new Date(2020, 1, 1, 13, 10, 0, 0),
-    },
-    {
-        id: 1,
-        title: 'Egon Spengler',
-        start: new Date(2020, 2, 1, 20, 0, 0, 0),
-        end: new Date(2020, 2, 1, 21, 0, 0, 0),
-    },
+  {
+    id: 0,
+    title: "Egon Spengler",
+    start: new Date(2020, 4, 1, 13, 33, 0, 0),
+    end: new Date(2020, 4, 1, 13, 54, 0, 0),
+  },
+  {
+    id: 1,
+    title: "Egon Spengler",
+    start: new Date(2020, 6, 1, 11, 11, 0, 0),
+    end: new Date(2020, 6, 1, 14, 0, 0, 0),
+  },
 
-    {
-        id: 2,
-        title: 'Egon Spengler',
-        start: new Date(2020, 2, 16, 7, 0, 0),
-        end: new Date(2020, 2, 16, 10, 30, 0),
-    },
+  {
+    id: 2,
+    title: "Egon Spengler",
+    start: new Date(2020, 5, 16, 17, 33, 0, 0),
+    end: new Date(2020, 5, 16, 20, 2, 0, 0),
+  },
 
-    {
-        id: 3,
-        title: 'Glinda Southgood',
-        start: new Date(2020, 1, 1, 17, 0, 0, 0),
-        end: new Date(2020, 1, 1, 17, 30, 0, 0),
-    },
+  {
+    id: 3,
+    title: "Glinda Southgood",
+    start: new Date(2020, 4, 13, 8, 0, 0, 0),
+    end: new Date(2020, 4, 13, 10, 0, 0, 0),
+  },
 
-    {
-        id: 4,
-        title: 'Glinda Southgood',
-        start: new Date(2020, 2, 1, 20, 0, 0, 0),
-        end: new Date(2020, 2, 1, 21, 0, 0, 0),
-    },
-    {
-        id: 5,
-        title: 'Glinda Southgood',
-        start: new Date(2020, 2, 16, 7, 0, 0),
-        end: new Date(2020, 2, 16, 10, 30, 0),
-        desc: 'Big conference for important people',
-    },
-    {
-        id: 6,
-        title: 'Laurel Fischer',
-        start: new Date(2020, 1, 1, 17, 0, 0, 0),
-        end: new Date(2020, 1, 1, 17, 30, 0, 0),
-        desc: 'Pre-meeting meeting, to prepare for the meeting',
-    },
-    {
-        id: 7,
-        title: 'Laurel Fischer',
-        start: new Date(2020, 2, 1, 20, 0, 0, 0),
-        end: new Date(2020, 2, 1, 21, 0, 0, 0),
-        desc: 'Power lunch',
-    },
-    {
-        id: 8,
-        title: 'Laurel Fischer',
-        start: new Date(2020, 2, 16, 7, 0, 0),
-        end: new Date(2020, 2, 16, 10, 30, 0),
-    },
-    {
-        id: 9,
-        title: 'Andrew Walsch',
-        start: new Date(2020, 1, 1, 17, 0, 0, 0),
-        end: new Date(2020, 1, 1, 17, 30, 0, 0),
-     },
-    {
-        id: 10,
-        title: 'Andrew Walsch',
-        start: new Date(2020, 2, 1, 20, 0, 0, 0),
-        end: new Date(2020, 2, 1, 21, 0, 0, 0),
-        desc: 'Power lunch',
-    },
-    {
-        id: 11,
-        title: 'Andrew Walsch',
-        start: new Date(2020, 2, 16, 7, 0, 0),
-        end: new Date(2020, 2, 16, 10, 30, 0),
-    },
-    {
-        id: 12,
-        title: 'Fred Flinstone',
-        start: new Date(2020, 1, 1, 17, 0, 0, 0),
-        end: new Date(2020, 1, 1, 17, 30, 0, 0),
-        desc: 'Most important meal of the day',
-    },
-    {
-        id: 13,
-        title: 'Fred Flinstone',
-        start: new Date(2020, 2, 1, 20, 0, 0, 0),
-        end: new Date(2020, 2, 1, 21, 0, 0, 0),
-    },
-    {
-        id: 14,
-        title: 'Fred Flinstone',
-        start: new Date(2020, 2, 16, 7, 0, 0),
-        end: new Date(2020, 2, 16, 10, 30, 0),
-    },
-     
-  ]
- 
-export default events
+  {
+    id: 4,
+    title: "Glinda Southgood",
+    start: new Date(2020, 6, 21, 5, 30, 0, 0),
+    end: new Date(2020, 6, 21, 7, 0, 0, 0),
+  },
+  {
+    id: 5,
+    title: "Glinda Southgood",
+    start: new Date(2020, 5, 6, 21, 33, 0),
+    end: new Date(2020, 5, 6, 21, 53, 0),
+    desc: "Big conference for important people",
+  },
+  {
+    id: 6,
+    title: "Laurel Fischer",
+    start: new Date(2020, 4, 20, 20, 20, 0, 0),
+    end: new Date(2020, 4, 20, 21, 54, 0, 0),
+    desc: "Pre-meeting meeting, to prepare for the meeting",
+  },
+  {
+    id: 7,
+    title: "Laurel Fischer",
+    start: new Date(2020, 6, 16, 11, 11, 0, 0),
+    end: new Date(2020, 6, 15, 12, 0, 0, 0),
+    desc: "Power lunch",
+  },
+  {
+    id: 8,
+    title: "Laurel Fischer",
+    start: new Date(2020, 5, 14, 18, 33, 0, 0),
+    end: new Date(2020, 5, 14, 20, 2, 0, 0),
+  },
+  {
+    id: 9,
+    title: "Andrew Walsch",
+    start: new Date(2020, 4, 12, 7, 33, 0, 0),
+    end: new Date(2020, 4, 12, 10, 44, 0, 0),
+  },
+  {
+    id: 10,
+    title: "Andrew Walsch",
+    start: new Date(2020, 6, 4, 12, 11, 0, 0),
+    end: new Date(2020, 6, 4, 14, 0, 0, 0),
+    desc: "Power lunch",
+  },
+  {
+    id: 11,
+    title: "Andrew Walsch",
+    start: new Date(2020, 5, 5, 15, 30, 0),
+    end: new Date(2020, 5, 5, 18, 30, 0),
+  },
+  {
+    id: 12,
+    title: "Fred Flinstone",
+    start: new Date(2020, 4, 30, 3, 15, 0, 0),
+    end: new Date(2020, 4, 30, 5, 30, 0, 0),
+    desc: "Most important meal of the day",
+  },
+  {
+    id: 13,
+    title: "Fred Flinstone",
+    start: new Date(2020, 6, 10, 7, 10, 0, 0),
+    end: new Date(2020, 6, 10, 8, 0, 0, 0),
+  },
+  {
+    id: 14,
+    title: "Fred Flinstone",
+    start: new Date(2020, 5, 22, 16, 30, 0, 0),
+    end: new Date(2020, 5, 22, 17, 0, 0, 0),
+  },
+];
+
+export default events;
